fcc44003cf1ba524b23163aa48048b38c4aab3b7
Ashwin20102005 <ashwinburela20@gmail.com>
2024-11-12 10:27:34 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util*;
+class main{
+  public static void main(string[]args){
+    Scanner sc = new Scanner(System.in);
+    int a=10;
+    int b=20;
+    System.out.println(a+b);
+    system.out.println(a*b);
+    system.out.println(a/b);
+    System.out.println(a-b);
+  }
+}
